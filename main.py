f82513e6dd1946dfbbfccddc20fee18814c40546
NatiTheFly <natalia_clavijo@biswashington.org>
2025-01-16 17:50:11 +0000

    i have allergies

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+with open("students.txt", "r") as file:
+    for line in file:
+        print(line.strip())
 
+with open("students.txt", "'r") as file:
+    for line in file:
+        print(line[2])
+
+with open("students.txt", "r") as file:
+    for line in file:
+        if len(line.strip()) > 6:
+            print(line.strip())
+new_line = input("enter a new name: ")
+
+with open("students.txt", "a") as file:
+    file.write(new_line + "\n")
